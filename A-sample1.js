de2e3be8fa161a32b7aa08ef3cdd645fe274669a
yukinameko <40497652+yukinameko@users.noreply.github.com>
2018-10-06 00:56:58 +0900

    sample1.jsをaddで実装していたために発生していなかったオーバーフローが生じるようにした

--- A-sample1.js
+++ A-sample1.js
@@ -1,9 +1,13 @@
 const cv = require('opencv4nodejs');
 
-const lena = cv.imread('image/lena.pgm');
-const mask = cv.imread('image/mask.pgm');
+const lena = cv.imread('image/lena.pgm', cv.CV_8UC1);
+const mask = cv.imread('image/mask.pgm', cv.CV_8UC1);
 
-const out = lena.add(mask);
+const mask_array = mask.getDataAsArray();
+const out_array = lena.getDataAsArray().map((v, i) =>
+	v.map((v, j) => v + mask_array[i][j])
+);
+const out = new cv.Mat(out_array, cv.CV_8UC1);
 
 cv.imwrite('outImage/out1.png', out);
 
