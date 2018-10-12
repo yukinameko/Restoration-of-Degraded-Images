d8f1cd909f8e4c7552ea94d709fa1d29ef4f8eab
yukinameko <40497652+yukinameko@users.noreply.github.com>
2018-10-12 18:01:54 +0900

    課題Cの画像劣化プログラムのC-1.jsを変更 11*11の斜め30度方向のブレのpsfをたたみ込むようにした．

--- C-1.js
+++ C-1.js
@@ -1,5 +1,7 @@
 const cv = require('opencv4nodejs');
-const N = 3;
+const fs = require('fs');
+
+const N = 11;
 const n = parseInt((N-1)/2);
 
 if(process.argv.length < 2){
@@ -7,16 +9,9 @@ if(process.argv.length < 2){
 	process.exit(1);
 }
 
-var pse;
-eval('pse = '+process.argv[3]);
-// console.log(pse[0]);
-if(pse === undefined || pse.length != pse[0].length)
-	process.exit(0);
-
 const image = cv.imread(process.argv[2], cv.CV_8UC1);
 
-// const pse = Array.from(new Array(N)).map(() => Array.from(new Array(N)).map(() => 1/(N*N)));
-// const pse = Array.from(new Array(N)).map(() => Array.from(new Array(N)).map((v, i) => i==n?1/N:0));
+const psf = JSON.parse(fs.readFileSync('motion.json', 'utf8'));
 
 const start = Date.now();
 const image_noise_array = image.getDataAsArray().map((v, i, a) => v.map((v, j) => {
@@ -26,7 +21,7 @@ const image_noise_array = image.getDataAsArray().map((v, i, a) => v.map((v, j) =
 	for(i-=n; i_ < N; i_++, i++){
 		var j_ = 0;
 		for(j-=n; j_ < N; j_++, j++){
-			sum += (i < 0 || i >= a.length || j < 0 || j >= a[0].length)?0:a[i][j]*pse[i_][j_];
+			sum += (i < 0 || i >= a.length || j < 0 || j >= a[0].length)?0:a[i][j]*psf[i_][j_];
 		}
 	}
 	i = i_org;
