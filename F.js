6dbb41ce5b2e8c5cdb17033a5614a4275b02a13f
yukinameko <g1813551@gmail.com>
2018-11-17 05:34:35 +0900

    edit F and matrixParseInt

--- F.js
+++ F.js
@@ -6,17 +6,20 @@ const tm = require('./timer.js');
 const D = require('./dots.js');
 const ps = require('./matrixParseInt.js');
 
-if(process.argv.length < 5){
-	console.log('error: not found argv'+process.argv.length+'\nnode F <input image path> <psf.json> <output image path>');
+if(process.argv.length < 6){
+	console.log('error: not found argv'+process.argv.length+'\nnode F <input image path> <psf.json> <output image path> <parseInt mode(0 or 1 or 2)>');
 	process.exit(1);
 }
+
 const argv = process.argv;
 
 const img = cv.imread(argv[2], cv.CV_8UC1);
 
 const psf = JSON.parse(fs.readFileSync(argv[3], 'utf8'));
 
-const out_path = argv[4]
+const out_path = argv[4];
+
+const parseInt_index = argv[5];
 
 // psf inverse
 const P = F.psf2P(psf, img.sizes);
@@ -26,7 +29,7 @@ const P_inv = math.transpose(tm.timer(() => math.inv(P)));
 const img_array = img.getDataAsArray();
 const img_array_vec = img_array.reduce((pre, cur) => {pre.push(...cur); return pre;}, []);
 
-const img_org_array_vec = ps.parseUInt8(tm.timer(() => D.dots(P_inv, img_array_vec)));
+const img_org_array_vec = ps[ps.mode[parseInt_index]](tm.timer(() => D.dots(P_inv, img_array_vec)));
 
 const img_org_array = Array.from(new Array(img.rows)).map((v, i) => img_org_array_vec.slice(i*img.cols, (i+1)*img.cols));
 
--- matrixParseInt.js
+++ matrixParseInt.js
@@ -3,3 +3,5 @@ exports.parseInt = A => A.map(v => v|0);
 exports.parseUInt = A => A.map(v => v<0?0:(v|0));
 
 exports.parseUInt8 = A => A.map(v => v<0?0:(v>255?255:(v|0)));
+
+exports.mode = ['parseInt', 'parseUInt', 'parseUInt8'];
