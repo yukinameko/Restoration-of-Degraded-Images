6755731878ab971273b861f121cc1d7d52ccc77b
yukinameko <g1813551@gmail.com>
2018-11-17 05:13:36 +0900

    edit E and add E ver.P

--- E_P.js
+++ E_P.js
@@ -1,5 +1,8 @@
 const cv = require('opencv4nodejs');
 const fs = require('fs');
+const F = require('./psf2P.js');
+const tm = require('./timer.js');
+const D = require('./dots.js');
 
 if(process.argv.length < 6){
 	console.log('error: not found argv'+process.argv.length+'\nnode E <input image path> <psf.json> <bit> <output image path>');
@@ -21,30 +24,19 @@ const n = parseInt((N-1)/2);
 
 const bit = parseInt(Array.from(new Array(8)).reduce((pre, cur, i) => ((i<bit_num)?'0':'1')+pre, ''), 2);
 
+const P = F.psf2P(psf, image.sizes);
+
+const img_array = image.getDataAsArray();
+const img_array_vec = img_array.reduce((pre, cur) => {pre.push(...cur); return pre;}, []);
+
 const start = Date.now();
-const image_noise_array = image.getDataAsArray().map((v, io, a) => v.map((v, jo) => {
-	var sum = 0.0;
-	var i = io - n;
-	var i_ = 0;
-	while(i_<N){
-		if(!(i < 0 || i >= a.length)){
-			var j_ = 0;
-			var j = jo - n;
-			while(j_<N){
-				sum += (j < 0 || j >= a[0].length)?0:(a[i][j]*psf[i_][j_]);
-				j++; j_++;
-			}
-		}
-		i++; i_++;
-	}
-
-	return (sum|0) & bit;
-}));
+const img_noise_vec = D.dots(P, img_array_vec);
 const end = Date.now();
-
 console.log((end - start)+'ms');
 
-const image_noise = new cv.Mat(image_noise_array, cv.CV_8UC1);
+const img_noise_array = Array.from(new Array(image.rows)).map((v, i) => img_noise_vec.slice(i*image.cols, (i+1)*image.cols));
+
+const image_noise = new cv.Mat(img_noise_array, cv.CV_8UC1);
 
 cv.imwrite('outImage/'+out_name, image_noise);
 
