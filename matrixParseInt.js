ce7bab067b059c6ee0abd88f889f435c3f076a82
yukinameko <g1813551@gmail.com>
2019-01-28 02:28:23 +0900

    edit matrixParseInt for matrix array

--- matrixParseInt.js
+++ matrixParseInt.js
@@ -1,7 +1,7 @@
-exports.parseInt = A => A.map(v => v|0);
+exports.parseInt = A => isFinite(A[0]) ? A.map(v => v|0) : A.map(v => v.map( v => v|0));
 
-exports.parseUInt = A => A.map(v => v<0?0:(v|0));
+exports.parseUInt = A => isFinite(A[0]) ? A.map(v => v<0?0:(v|0)) : A.map(v => v.map(v => v<0?0:(v|0)));
 
-exports.parseUInt8 = A => A.map(v => v<0?0:(v>255?255:(v|0)));
+exports.parseUInt8 = A => isFinite(A[0]) ? A.map(v => v<0?0:(v>255?255:(v|0))) : A.map(v => v.map(v => v<0?0:(v>255?255:(v|0))));
 
 exports.mode = ['parseInt', 'parseUInt', 'parseUInt8'];
